17209ddec7fa4047dfe76206dc873ca041ba7a8f
lohcy <loh.cheeyung96@gmail.com>
2024-02-28 17:05:54 +0800

    create routes folder

---
+++ lib/routes/names.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const INITIAL = '/';
+  static const INTRODUCTION = '/introduction';
+  static const SIGN_IN = '/sign_in';
+ static const HOME = '/home';
+  
+}
---
+++ lib/routes/pages.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'routes.dart';
+
+class AppPages {
+  static const INITIAL = AppRoutes.INITIAL;
+  static final RouteObserver<Route> observer = RouteObservers();
+  static List<String> history = [];
+
+  static final List<GetPage> routes = [
+    // GetPage(
+    //   name: AppRoutes.INITIAL,
+    //   page: () => const DataUploaderScreen(),
+    //   binding: DataUploaderBinding(),
+    //   customTransition: CustomScaleTransition(),
+    //   transitionDuration: Duration(milliseconds: 500),
+    // ),
+
+    // GetPage(
+    //   name: AppRoutes.INITIAL,
+    //   transitionDuration: Duration(milliseconds: 500),
+    // ),
+    // GetPage(
+      // name: AppRoutes.HOME,
+      // name: AppRoutes.HOME,
+      // page: () => const HomePage(),
+      // binding: BindingsBuilder(() {
+      //   Get.put(QuestionPaperController());
+      //   Get.put(MyZoomDrawerController());
+      // }),
+      // customTransition: CustomScaleTransition(),
+      // transitionDuration: Duration(milliseconds: 500),
+  ];
+}
