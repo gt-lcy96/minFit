529bfbe6e2eb94be0448882586fb47c32f7c5d72
lohcy <loh.cheeyung96@gmail.com>
2024-02-29 14:04:43 +0800

    init pages

---
+++ lib/pages/more_page/view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class MorePage extends StatelessWidget {
+  const MorePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('More Page'));
+  }
+}
