48f33a2d17012d6a9469bb50704fb34732680cf1
lohcy <loh.cheeyung96@gmail.com>
2024-02-29 12:08:13 +0800

    create Calories chart

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,4 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const onSurfaceTextColor = Colors.white;
+  static const primaryLightColor = Color.fromARGB(255, 106, 240, 224);
+
+  static const Color primaryDarkColorDark = Color.fromARGB(255, 46, 52, 98);
+  static const Color primaryColorDark = Color.fromARGB(255, 153, 179, 225);
+
+  static const mainGradientDark = LinearGradient(
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+    colors: [primaryDarkColorDark, primaryColorDark]);
 }
--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:min_fitness/constants/colors.dart';
+import 'package:min_fitness/widgets/appIcon_text.dart';
+import 'package:min_fitness/widgets/colored_text.dart';
 import 'package:min_fitness/widgets/shadow_card.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
@@ -20,13 +22,70 @@ class HomePage extends StatelessWidget {
             margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
             child: ShadowedCard(
               width: Get.width * 0.8,
-              child: CircularPercentIndicator(
-                radius: 60.0,
-                lineWidth: 15.0,
-                percent: 0.8,
-                center: new Text("80%"),
-                progressColor: Color.fromARGB(255, 84, 147, 255),
-                circularStrokeCap: CircularStrokeCap.round,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  ColoredText(
+                    text: 'Calories',
+                    fontSize: 20,
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AppIconText(
+                          icon: Icon(Icons.outlined_flag,
+                              color: AppColors.onSurfaceTextColor),
+                          text: Column(
+                            children: [
+                              ColoredText(text: 'Target', fontSize: 12),
+                              ColoredText(
+                                text: '1510',
+                                fontSize: 12,
+                              ),
+                            ],
+                          )),
+                      SizedBox(width: 10.w),
+                      CircularPercentIndicator(
+                        radius: 60.0,
+                        lineWidth: 15.0,
+                        percent: 0.8,
+                        center: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Remaining",
+                                style: TextStyle(
+                                    color: AppColors.onSurfaceTextColor)),
+                            Text("1156",
+                                style: TextStyle(
+                                    color: AppColors.onSurfaceTextColor)),
+                          ],
+                        ),
+                        progressColor: AppColors.primaryLightColor,
+                        circularStrokeCap: CircularStrokeCap.round,
+                      ),
+                      SizedBox(width: 10.w),
+                      AppIconText(
+                        icon: Icon(Icons.restaurant,
+                            color: AppColors.onSurfaceTextColor),
+                        text: Column(
+                          children: [
+                            ColoredText(text: 'Eaten', fontSize: 12),
+                            ColoredText(
+                              text: '${1510 - 1156}',
+                              fontSize: 12,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           )
