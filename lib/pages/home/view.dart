1352e5cf90686920689620ccd77e8ab584243efb
lohcy <loh.cheeyung96@gmail.com>
2024-02-29 13:30:28 +0800

    add SingleChildScrollView and adjust spacing

--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -13,31 +13,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-            child: caloriesContent(),
-          ),
-          SizedBox(
-            height: 20.h,
-          ),
-          Container(
-            // margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-            child: addExcerciseContent(),
-          ),
-          SizedBox(
-            height: 20.h
-          ),
-          ShadowedCard(
-            width: Get.width * 0.8,
-            height: 180,
-            paddingAll: 0,
-            child: LineChartSample2(),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 5.h),
+              child: caloriesContent(),
+            ),
+            SizedBox(
+              height: 20.h,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 5.h),
+              child: addExcerciseContent(),
+            ),
+            SizedBox(
+              height: 20.h
+            ),
+            ShadowedCard(
+              width: Get.width * 0.8,
+              height: 180,
+              paddingAll: 0,
+              child: LineChartSample2(),
+            ),
+          ],
+        ),
       ),
     );
   }
