20da4c4c306eedb66bc3981a8f9f23d27b01f5d9
lohcy <loh.cheeyung96@gmail.com>
2024-02-29 14:59:40 +0800

    add gradient color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -7,8 +7,18 @@ class AppColors {
   static const Color primaryDarkColorDark = Color.fromARGB(255, 46, 52, 98);
   static const Color primaryColorDark = Color.fromARGB(255, 153, 179, 225);
 
+  static const Color primaryLightColorLight = Color.fromARGB(255, 58, 203, 176);
+  static const Color primaryColorLight = Color.fromARGB(255, 248, 81, 165);
+
+  static const mainGradientLight = LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [primaryLightColorLight, primaryColorLight]);
+
   static const mainGradientDark = LinearGradient(
-    begin: Alignment.topRight,
-    end: Alignment.bottomLeft,
-    colors: [primaryDarkColorDark, primaryColorDark]);
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [primaryDarkColorDark, primaryColorDark]);
+
+  static LinearGradient mainGradient() => mainGradientDark;
 }
