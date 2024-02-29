44d2b7f3c6e099c83d58d90f9d5c2d86a12d5a8c
lohcy <loh.cheeyung96@gmail.com>
2024-02-29 12:13:27 +0800

    refactor into caloriesContent

--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -20,77 +20,78 @@ class HomePage extends StatelessWidget {
         children: [
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-            child: ShadowedCard(
-              width: Get.width * 0.8,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  ColoredText(
-                    text: 'Calories',
-                    fontSize: 20,
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      AppIconText(
-                          icon: Icon(Icons.outlined_flag,
-                              color: AppColors.onSurfaceTextColor),
-                          text: Column(
-                            children: [
-                              ColoredText(text: 'Target', fontSize: 12),
-                              ColoredText(
-                                text: '1510',
-                                fontSize: 12,
-                              ),
-                            ],
-                          )),
-                      SizedBox(width: 10.w),
-                      CircularPercentIndicator(
-                        radius: 60.0,
-                        lineWidth: 15.0,
-                        percent: 0.8,
-                        center: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text("Remaining",
-                                style: TextStyle(
-                                    color: AppColors.onSurfaceTextColor)),
-                            Text("1156",
-                                style: TextStyle(
-                                    color: AppColors.onSurfaceTextColor)),
-                          ],
-                        ),
-                        progressColor: AppColors.primaryLightColor,
-                        circularStrokeCap: CircularStrokeCap.round,
-                      ),
-                      SizedBox(width: 10.w),
-                      AppIconText(
-                        icon: Icon(Icons.restaurant,
-                            color: AppColors.onSurfaceTextColor),
-                        text: Column(
-                          children: [
-                            ColoredText(text: 'Eaten', fontSize: 12),
-                            ColoredText(
-                              text: '${1510 - 1156}',
-                              fontSize: 12,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            child: caloriesContent(),
           )
         ],
       ),
     );
   }
+}
+
+Widget caloriesContent() {
+  return ShadowedCard(
+    width: Get.width * 0.8,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 20.h,
+        ),
+        ColoredText(
+          text: 'Calories',
+          fontSize: 20,
+        ),
+        SizedBox(
+          height: 20.h,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AppIconText(
+                icon: Icon(Icons.outlined_flag,
+                    color: AppColors.onSurfaceTextColor),
+                text: Column(
+                  children: [
+                    ColoredText(text: 'Target', fontSize: 12),
+                    ColoredText(
+                      text: '1510',
+                      fontSize: 12,
+                    ),
+                  ],
+                )),
+            SizedBox(width: 10.w),
+            CircularPercentIndicator(
+              radius: 60.0,
+              lineWidth: 15.0,
+              percent: 0.8,
+              center: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Remaining",
+                      style: TextStyle(color: AppColors.onSurfaceTextColor)),
+                  Text("1156",
+                      style: TextStyle(color: AppColors.onSurfaceTextColor)),
+                ],
+              ),
+              progressColor: AppColors.primaryLightColor,
+              circularStrokeCap: CircularStrokeCap.round,
+            ),
+            SizedBox(width: 10.w),
+            AppIconText(
+              icon: Icon(Icons.restaurant, color: AppColors.onSurfaceTextColor),
+              text: Column(
+                children: [
+                  ColoredText(text: 'Eaten', fontSize: 12),
+                  ColoredText(
+                    text: '${1510 - 1156}',
+                    fontSize: 12,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
 }
