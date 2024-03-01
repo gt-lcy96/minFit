96bf941fd63b2364a939ead153dff36e8c064386
lohcy <loh.cheeyung96@gmail.com>
2024-03-01 13:42:03 +0800

    use exercise list to generate GridView

--- lib/pages/workout_page/view.dart
+++ lib/pages/workout_page/view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:min_fitness/constants/colors.dart';
+import 'package:min_fitness/mock_data/exercise_list.dart';
 import 'package:min_fitness/widgets/shadow_card.dart';
 
 class Workout extends StatelessWidget {
@@ -18,38 +19,13 @@ class Workout extends StatelessWidget {
               crossAxisSpacing: 10,
               mainAxisSpacing: 10,
               crossAxisCount: 2,
-              children:
-               <Widget>[
-                ShadowedCard(
+              children: ExerciseList().exercises.map((exercise) {
+
+                return ShadowedCard(
                     backgroundColor: AppColors.onSurfaceTextColor,
                     child: networkImage(
-                        'https://api.exercisedb.io/image/6s2UKJZxr9Lub4')),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.teal[200],
-                  child: const Text('Heed not the rabble'),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.teal[300],
-                  child: const Text('Sound of screams but the'),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.teal[400],
-                  child: const Text('Who scream'),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.teal[500],
-                  child: const Text('Revolution is coming...'),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.teal[600],
-                  child: const Text('Revolution, they...'),
-                ),
-              ],
+                        exercise['gifUrl']));
+              }).toList(),
             ),
           )
         ],
