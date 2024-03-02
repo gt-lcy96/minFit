5774a8acc2ee6c2f8c82e737185369b04e9d6902
lohcy <loh.cheeyung96@gmail.com>
2024-03-02 15:51:11 +0800

    initial layout for workout page

--- lib/pages/workout_page/view.dart
+++ lib/pages/workout_page/view.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:min_fitness/constants/colors.dart';
 import 'package:min_fitness/mock_data/exercise_list.dart';
 import 'package:min_fitness/models/exercise_model.dart';
@@ -14,22 +16,42 @@ class Workout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: GridView.count(
-              primary: false,
-              padding: const EdgeInsets.all(20),
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              crossAxisCount: 2,
-              children: ExerciseList().exercises.map((exercise) {
-                final exerciseContent = ExerciseModel.fromJson(json.encode(exercise));
-                return ExerciseCard(content:exerciseContent);
-              }).toList(),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 0.h,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  width: Get.width,
+                  height: Get.height * 0.5,
+                  child: GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 2,
+                    children: ExerciseList().exercises.map((exercise) {
+                      final exerciseContent = ExerciseModel.fromJson(json.encode(exercise));
+                      return ExerciseCard(content:exerciseContent);
+                    }).toList(),
+                  ),
+                ),
+        
             ),
-          )
-        ],
+            Positioned(
+              top: 20,
+              left: 20,
+              right: 20,
+              child: ShadowedCard(
+                height: Get.height * 0.35,
+                backgroundColor: AppColors.primaryLightColor.withOpacity(0.4),
+              child: Text('testing'),
+            )),
+          ],
+        ),
       ),
     );
   }
