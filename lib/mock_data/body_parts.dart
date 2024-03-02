d8a257563ed0afe7f745129bf04624158fb9b490
lohcy <loh.cheeyung96@gmail.com>
2024-03-02 14:19:35 +0800

    add more mock data

---
+++ lib/mock_data/body_parts.dart
@@ -0,0 +1,14 @@
+class BodyPart {
+  final body_part_list = [
+    "back",
+    "cardio",
+    "chest",
+    "lower arms",
+    "lower legs",
+    "neck",
+    "shoulders",
+    "upper arms",
+    "upper legs",
+    "waist",
+  ];
+}
---
+++ lib/mock_data/target_muscle.dart
@@ -0,0 +1,23 @@
+class TargetMuscle {
+  final muscle_list = [
+    "abductors",
+    "abs",
+    "adductors",
+    "biceps",
+    "calves",
+    "cardiovascular system",
+    "delts",
+    "forearms",
+    "glutes",
+    "hamstrings",
+    "lats",
+    "levator scapulae",
+    "pectorals",
+    "quads",
+    "serratus anterior",
+    "spine",
+    "traps",
+    "triceps",
+    "upper back"
+  ];
+}
