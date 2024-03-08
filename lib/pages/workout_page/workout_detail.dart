cef5f04ce43ea612fa645c17bcaac880d4fd43d8
lohcy <loh.cheeyung96@gmail.com>
2024-03-08 16:41:02 +0800

    use textRow to avoid TextOverFlow

--- lib/pages/workout_page/workout_detail.dart
+++ lib/pages/workout_page/workout_detail.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:min_fitness/constants/colors.dart';
 import 'package:min_fitness/constants/constant.dart';
 import 'package:min_fitness/controllers/exercise_controller.dart';
+import 'package:min_fitness/models/exercise_model.dart';
 import 'package:min_fitness/pages/workout_page/view.dart';
 import 'package:min_fitness/widgets/appIcon_text.dart';
 import 'package:min_fitness/widgets/colored_text.dart';
@@ -70,85 +71,43 @@ class WorkoutDetail extends StatelessWidget {
                                   Icons.accessibility_new,
                                   size: 50,
                                 ),
-                                text: Text(
-                                  'Body Part : ${exercise_detail!.bodyPart}',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                )),
-                            SizedBox(
-                              height: 10.h,
-                            ),
+                                text: textRow(
+                                    'Body Part: ', exercise_detail.bodyPart)),
+                            columnDivider(),
                             // equipment
                             AppIconText(
                                 icon: Icon(
                                   Icons.fitness_center,
                                   size: 50,
                                 ),
-                                text: Text(
-                                  'Equipment : ${exercise_detail!.equipment}',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                )),
-                            SizedBox(
-                              height: 10.h,
-                            ),
+                                text: textRow(
+                                    'Equipment: ', exercise_detail.equipment)),
+                            columnDivider(),
                             // Train target
                             AppIconText(
                                 icon: Icon(
                                   Icons.outlined_flag,
                                   size: 50,
                                 ),
-                                text: Text(
-                                  'Train Target: ${exercise_detail!.target}',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                )),
-                            SizedBox(
-                              height: 10.h,
-                            ),
+                                text: textRow(
+                                    'Train Target: ', exercise_detail.target)),
+                            columnDivider(),
                             // Train target
                             AppIconText(
                                 icon: Icon(
                                   Icons.local_fire_department,
                                   size: 50,
                                 ),
-                                text: Text(
-                                  'Calories burn per hour: ${exercise_detail!.metValue}',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                )),
-                            SizedBox(height: 60.h),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                InkWell(
-                                  onTap: () {print('detail tapped');},
-                                  child: Container(
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: 20.h, horizontal: 60.w),
-                                    child: Text('Add Exercise', style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),),
-                                    decoration: BoxDecoration(
-                                      color: AppColors.primaryLightColor,
-                                      borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Colors.grey.withOpacity(0.5),
-                                          spreadRadius: 5,
-                                          blurRadius: 7,
-                                          offset: Offset(0,
-                                              3), // changes position of shadow
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
+                                text: textRow('Calories / hour: ',
+                                    exercise_detail.metValue.toString())),
+                            SizedBox(height: 10.h),
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  AddExerciseButton(),
+                                ],
+                              ),
                             )
                           ],
                         )),
@@ -163,4 +122,69 @@ class WorkoutDetail extends StatelessWidget {
       }),
     );
   }
+}
+
+Widget columnDivider() {
+  return Divider(
+    height: 10.h,
+    indent: 10.w,
+    endIndent: 10.w,
+  );
+}
+
+Widget textRow(String title, String description) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      Container(
+        width: Get.width * 0.3,
+        child: Text(
+          title,
+          overflow: TextOverflow.clip,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+      Container(
+        width: Get.width * 0.4,
+        child: Text(
+          description,
+          overflow: TextOverflow.clip,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget AddExerciseButton() {
+  return InkWell(
+    onTap: () {
+      print('detail tapped');
+    },
+    child: Container(
+      padding: EdgeInsets.symmetric(vertical: 20.h, horizontal: 60.w),
+      child: Text(
+        'Add Exercise',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.primaryLightColor,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+    ),
+  );
 }
