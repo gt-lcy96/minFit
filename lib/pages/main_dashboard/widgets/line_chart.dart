68c3c1b549f46189e321d78c016489fc8a15de1d
lohcy <loh.cheeyung96@gmail.com>
2024-03-11 14:14:33 +0800

    set y data (weight) for linechart

--- lib/pages/main_dashboard/widgets/line_chart.dart
+++ lib/pages/main_dashboard/widgets/line_chart.dart
@@ -1,7 +1,9 @@
 // import 'package:fl_chart_app/presentation/resources/app_resources.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:min_fitness/constants/colors.dart';
+import 'dart:math';
 
 class LineChartWeight extends StatefulWidget {
   const LineChartWeight({super.key});
@@ -20,53 +22,133 @@ class LineChartWeightState extends State<LineChartWeight> {
 
   final Color mainGridLineColor = Colors.white10;
 
-  bool showAvg = true;
 
   final double minX = 0;
   final double maxX = 10;
-  late final List<FlSpot> lineBarData = [
-    FlSpot(minX, 2),
-    FlSpot(2.6, 2),
-    FlSpot(4.9, 2),
-    FlSpot(6.8, 3.1),
-    FlSpot(8, 4),
-    FlSpot(9.5, 4),
-    FlSpot(maxX, 5),
+
+  // late final List<FlSpot> lineBarData = [
+  //   FlSpot(1, 72),
+  //   FlSpot(2, 74),
+  //   FlSpot(3, 78),
+  //   FlSpot(4, 76),
+  //   FlSpot(5, 72),
+  //   FlSpot(6, 74),
+  // ];
+
+  final date_x = {2: '15/01', 4: '15/02', 6: '15/03'};
+
+  final List<double> weight_y = [
+    64,
+    74,
+    74,
+    73,
+    74,
+    75,
   ];
+
+  late List<double> y_range = [];
+
+  late double weight_max = 100;
+  late double weight_min = 0;
+  late int std = 0;
+  List y_marks = [];
+  int y_mark_counter = 0;
+  late List<FlSpot> lineBarData;
+
+  double calculateAverage(List<num> numbers) {
+    if (numbers.isEmpty) {
+      return 0; // Handle the case when the list is empty to avoid division by zero
+    }
+
+    var sum = numbers.reduce((a, b) => a + b);
+    return sum / numbers.length;
+  }
+
+  double calculateStandardDeviation(List<double> data) {
+    // Compute the mean (average) of the data set
+    double mean = data.reduce((a, b) => a + b) / data.length;
+
+    // Compute the squared differences from the mean
+    var squaredDifferences = data.map((value) => pow(value - mean, 2));
+
+    // Compute the average of the squared differences (variance)
+    double variance =
+        squaredDifferences.reduce((a, b) => a + b) / (data.length - 1);
+
+    // Take the square root of the variance to get the standard deviation
+    double standardDeviation = sqrt(variance);
+
+    return standardDeviation;
+  }
+
+  double find_max_in_list(List<double> numbers) {
+    return numbers.reduce(
+        (double value, double element) => value > element ? value : element);
+  }
+
+  double find_min_in_list(List<double> numbers) {
+    return numbers.reduce(
+        (double value, double element) => value < element ? value : element);
+  }
+
+  double calculate_weight_ratio(weight, max_with_bias) {
+    return weight / max_with_bias;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+  int lineBarCounter = 0;
+  lineBarData = weight_y.map((y) {
+    lineBarCounter ++;
+    return FlSpot(lineBarCounter.toDouble(), y);
+  }).toList();
+
+
+    std = calculateStandardDeviation(weight_y).round();
+    weight_max = find_max_in_list(weight_y);
+    weight_min = find_min_in_list(weight_y);
+
+    double current_y_in_y_mark = weight_min - std;
+    while (current_y_in_y_mark <= weight_max + std) {
+      y_marks.add(current_y_in_y_mark);
+      current_y_in_y_mark += std;
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         AspectRatio(
-          aspectRatio: 1.70,
+          aspectRatio: 1.78,
           child: Padding(
             padding: const EdgeInsets.only(
               right: 18,
               left: 12,
-              top: 24,
+              // top: 24,
+              top: 36,
               bottom: 12,
             ),
-            child: LineChart(
-              // showAvg ? avgData() : mainData(),
-              mainData(),
+              child: LineChart(
+                // showAvg ? avgData() : mainData(),
+                mainData(),
+              ),
             ),
-          ),
         ),
         SizedBox(
           width: 60,
           height: 34,
           child: TextButton(
             onPressed: () {
-              setState(() {
-                showAvg = !showAvg;
-              });
             },
             child: Text(
               'Wght',
               style: TextStyle(
                 fontSize: 12,
-                color: showAvg ? Colors.white.withOpacity(0.5) : Colors.white,
+                color: Colors.white.withOpacity(0.5),
               ),
             ),
           ),
@@ -110,21 +192,23 @@ class LineChartWeightState extends State<LineChartWeight> {
       color: AppColors.onSurfaceTextColor,
     );
     String text;
-    switch (value.toInt()) {
-      case 1:
-        text = '70';
-        break;
-      case 3:
-        text = '72';
-        break;
-      case 5:
-        text = '74';
-        break;
-      default:
-        return Container();
+
+    if (value == y_marks[y_mark_counter]) {
+      if (y_mark_counter < y_marks.length - 1) {
+        y_mark_counter++;
+      }
+      text = value.toInt().toString();
+      return Text(text, style: style, textAlign: TextAlign.left);
     }
 
-    return Text(text, style: style, textAlign: TextAlign.left);
+    // if (value >= 70 && value <= 80 && ) {
+    //     text = value.toInt().toString();
+    //     return Text(text, style: style, textAlign: TextAlign.left);
+    //   }
+    else {
+      return Container();
+    }
+    // return Text(text, style: style, textAlign: TextAlign.left);
   }
 
   LineChartData mainData() {
@@ -178,8 +262,8 @@ class LineChartWeightState extends State<LineChartWeight> {
       ),
       minX: minX,
       maxX: maxX,
-      minY: 0,
-      maxY: 6,
+      minY: y_marks.first,
+      maxY: y_marks.last,
       lineBarsData: [
         LineChartBarData(
           spots: lineBarData,
@@ -198,103 +282,6 @@ class LineChartWeightState extends State<LineChartWeight> {
               colors: gradientColors
                   .map((color) => color.withOpacity(0.3))
                   .toList(),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  LineChartData avgData() {
-    return LineChartData(
-      lineTouchData: const LineTouchData(enabled: false),
-      gridData: FlGridData(
-        show: true,
-        drawHorizontalLine: true,
-        verticalInterval: 1,
-        horizontalInterval: 1,
-        getDrawingVerticalLine: (value) {
-          return const FlLine(
-            color: Color(0xff37434d),
-            strokeWidth: 1,
-          );
-        },
-        getDrawingHorizontalLine: (value) {
-          return const FlLine(
-            color: Color(0xff37434d),
-            strokeWidth: 1,
-          );
-        },
-      ),
-      titlesData: FlTitlesData(
-        show: true,
-        bottomTitles: AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: true,
-            reservedSize: 30,
-            getTitlesWidget: bottomTitleWidgets,
-            interval: 1,
-          ),
-        ),
-        leftTitles: AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: true,
-            getTitlesWidget: leftTitleWidgets,
-            reservedSize: 42,
-            interval: 1,
-          ),
-        ),
-        topTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        rightTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-      ),
-      borderData: FlBorderData(
-        show: true,
-        border: Border.all(color: const Color(0xff37434d)),
-      ),
-      minX: 0,
-      maxX: 11,
-      minY: 0,
-      maxY: 6,
-      lineBarsData: [
-        LineChartBarData(
-          spots: const [
-            FlSpot(0, 3.44),
-            FlSpot(2.6, 3.44),
-            FlSpot(4.9, 3.44),
-            FlSpot(6.8, 3.44),
-            FlSpot(8, 3.44),
-            FlSpot(9.5, 3.44),
-            FlSpot(11, 3.44),
-          ],
-          isCurved: true,
-          gradient: LinearGradient(
-            colors: [
-              ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                  .lerp(0.2)!,
-              ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                  .lerp(0.2)!,
-            ],
-          ),
-          barWidth: 5,
-          isStrokeCapRound: true,
-          dotData: const FlDotData(
-            show: false,
-          ),
-          belowBarData: BarAreaData(
-            show: true,
-            gradient: LinearGradient(
-              colors: [
-                ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                    .lerp(0.2)!
-                    .withOpacity(0.1),
-                ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                    .lerp(0.2)!
-                    .withOpacity(0.1),
-              ],
             ),
           ),
         ),
