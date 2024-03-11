02bcf824fd8923f6def98198166bf23530ff0adc
lohcy <loh.cheeyung96@gmail.com>
2024-03-11 14:53:21 +0800

    create mock data WeightOnDate

---
+++ lib/mock_data/weight_with_date.dart
@@ -0,0 +1,16 @@
+class WeightOnDate {
+  final weightDate = [
+    {
+      'date': '15/01/2024',
+      'weight': 72,
+    },
+    {
+      'date': '16/02/2024',
+      'weight': 73,
+    },
+    {
+      'date': '17/03/2024',
+      'weight': 75,
+    }
+  ];
+}
